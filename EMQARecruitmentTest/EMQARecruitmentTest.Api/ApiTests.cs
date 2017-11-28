58ea92ceef236f1fba2153d23c6de6f2013ba3cc
Wait <SWait@euromoneyplc.com>
2017-11-28 15:13:57 +0000

    initial commit

---
+++ EMQARecruitmentTest/EMQARecruitmentTest.Api/ApiTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EMQARecruitmentTest.Api
+{
+    [TestFixture]
+    public class ApiTests
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your API test code here
+            Assert.That(true, Is.True);
+        }
+    }
+}
